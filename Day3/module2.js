a87f0fd91cb4a8b8f8d4c71dc1fba719e22af2af
Phani Madhuri <phanimadhuri006@gmail.com>
2026-03-08 12:42:13 +0530

    Removed files from repository

--- Day3/module2.js
+++
@@ -1,7 +0,0 @@
-import
-// import a from './module1.js'
-// console.log('a is',a)
-// console.log(a.data)
-// console.log(a.username)
-// console.log(a.person)
-{data,person,username} from'./module1.js'
