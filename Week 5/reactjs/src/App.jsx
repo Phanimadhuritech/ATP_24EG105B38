f498a16530d8f9234e14d824edda1add02cbd8e8
Phani Madhuri <phanimadhuri006@gmail.com>
2026-03-24 23:24:58 +0530

    Added new files for Week 4 and Week 5

---
+++ Week 5/reactjs/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import UsersList from '../components/UsersList' 
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer' 
+
+function App(){
+  return(
+    <div>
+      <NavBar />
+      <div className='m-10 min-h-screen'>
+      <UsersList /></div>
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App
